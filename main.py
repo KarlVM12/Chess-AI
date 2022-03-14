a8095f6e29ad58816f9fb8594f4e7dd6d90f7964
Karl Muller <karlmuller10@gmail.com>
2022-03-14 12:08:44 -0400

    pygame basics

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("hello")
+import pygame
+pygame.init()
+
+screen = pygame.display.set_mode([500,500])
+
+running = True
+while running:
+
+    # "X"-ed window out
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    # Fill the background with white
+    screen.fill((255, 255, 255))
+
+
+    # Draw a solid blue circle in the center
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+
+    # updates the screen every frame
+    pygame.display.flip()
+
+# Done! Time to quit.
+pygame.quit()
